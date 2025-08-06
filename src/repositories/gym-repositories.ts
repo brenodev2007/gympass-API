69e8d0857e08dff52160119aeb14dfb403fc1785
Breno Soriani <breno.soriani@gmail.com>
2025-08-06 17:07:31 -0300

    Caso de uso de busca de academias

--- src/repositories/gym-repositories.ts
+++ src/repositories/gym-repositories.ts
@@ -3,4 +3,6 @@ import { Prisma, Gym } from "generated/prisma";
 export interface GymRepositories {
   findById(id: string): Promise<Gym | null>;
   create(data: Prisma.GymCreateInput): Promise<Gym>;
+
+  searchMany(query: string, page: number): Promise<Gym[]>;
 }
---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,24 @@
+import { GymRepositories } from "@/repositories/gym-repositories";
+import { Gym } from "../../generated/prisma";
+
+interface SearchGymUseCaseRequest {
+  query: string;
+  page: number;
+}
+
+interface SearchGymUseCaseResponse {
+  gyms: Gym[];
+}
+
+export class SearchGymUseCase {
+  constructor(private gymRepository: GymRepositories) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymUseCaseRequest): Promise<SearchGymUseCaseResponse> {
+    const gyms = await this.gymRepository.searchMany(query, page);
+
+    return { gyms };
+  }
+}
